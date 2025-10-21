2c5c45f3d356015774ef86b75e5a7b7eb75f177a
karlapraveenkumar <131789praveen@gmail.com>
2025-10-21 22:50:05 +0530

    Created Backend files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 // import 'bootstrap/dist/css/bootstrap.min.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  createRoutesFromElements,
+  Route
+} from 'react-router-dom';
 import './assets/styles/bootstrap.custom.css'
 import './assets/styles/index.css'
 import App from './App.jsx'
+import HomeSecreen from './screens/HomeSecreen.jsx';
+import ProductScreen from './screens/ProductScreen.jsx';
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true } path='/' element={<HomeSecreen/>} />
+      <Route path='/product/:id' element={<ProductScreen/>} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
