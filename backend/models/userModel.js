8508ff79702af8deea0f2e97a55b19bf320d02e6
karlapraveenkumar <131789praveen@gmail.com>
2025-10-24 10:29:35 +0530

    Modified the Seeder code

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,60 +1,26 @@
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
 
-// const userSchema = mongoose.Schema({
-//     name : {
-//         type : String,
-//         required : true
-//     },
-//     email : {
-//         type : String,
-//         required : true,
-//         unique : true
-//     },
-//     password : {
-//         type : String,
-//         required : true,
-//     },
-//     isAdmin : {
-//         type : Boolean,
-//         required : true,
-//         default : false,
-//     }
-// }, {timestamps : true});
-
-
-// const User = mongoose.model('User', userSchema);
-
-// export default User;
-
-
-import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
-
-const userSchema = mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
+const userSchema = mongoose.Schema({
+    name : {
+        type : String,
+        required : true
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
+    email : {
+        type : String,
+        required : true,
+        unique : true
     },
-    password: {
-      type: String,
-      required: true,
+    password : {
+        type : String,
+        required : true,
     },
-    isAdmin: {
-      type: Boolean,
-      required: true,
-      default: false,
-    },
-  },
-  {
-    timestamps: true,
-  }
-);
+    isAdmin : {
+        type : Boolean,
+        required : true,
+        default : false,
+    }
+}, {timestamps : true});
+
 
 const User = mongoose.model('User', userSchema);
 
