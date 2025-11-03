974b0d42173c9b7e27891840a938859b7b7040ed
karlapraveenkumar <131789praveen@gmail.com>
2025-11-03 14:02:32 +0530

    User Router and Controller Created

---
+++ backend/controllers/userController.js
@@ -0,0 +1,90 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import User from "../models/productModel.js";
+
+// @desc Auth user and get token
+// @route POST /api/users/login
+// @access Public
+
+const authUser = asyncHandler(async(req,res)=>{
+    res.send('Auth User');
+});
+
+
+// @desc Register a new user
+// @route POST /api/users
+// @access Public
+
+const registerUser = asyncHandler(async(req,res)=>{
+    res.send('Register User');
+});
+
+
+// @desc Logout user / clear cookie
+// @route POST /api/users/logout
+// @access Private
+
+const logoutUser = asyncHandler(async(req,res)=>{
+    res.send('Logout User');
+});
+
+
+// @desc get user profile
+// @route GET /api/users/profile
+// @access Public
+
+const getUserProfile = asyncHandler(async(req,res)=>{
+    res.send('Get User Profile');
+});
+
+// @desc Update user profile
+// @route PUT /api/users/profile
+// @access Private
+
+const updateUserProfile = asyncHandler(async(req,res)=>{
+    res.send('Update User Profile');
+});
+
+
+// @desc Get users
+// @route GET /api/users
+// @access Private/Admin
+
+const getUsers = asyncHandler(async(req,res)=>{
+    res.send('Get Users');
+});
+
+// @desc Get users by ID
+// @route GET /api/users/:id
+// @access Private/Admin
+
+const getUserById = asyncHandler(async(req,res)=>{
+    res.send('Get User By ID');
+});
+
+// @desc Delete users
+// @route DELETE /api/users/:id
+// @access Private/Admin
+
+const deleteUser = asyncHandler(async(req,res)=>{
+    res.send('Delete Users');
+});
+
+// @desc Update user
+// @route PUT /api/users/:id
+// @access Private/Admin
+
+const updateUser = asyncHandler(async(req,res)=>{
+    res.send('Update User');
+});
+
+export {
+    authUser, 
+    registerUser, 
+    logoutUser, 
+    getUserProfile, 
+    updateUserProfile, 
+    getUsers, 
+    getUserById, 
+    deleteUser, 
+    updateUser
+};
