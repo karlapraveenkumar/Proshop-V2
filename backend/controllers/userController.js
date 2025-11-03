85424e725e0cf83320d058d91d8e9a3752082dd5
karlapraveenkumar <131789praveen@gmail.com>
2025-11-03 19:35:49 +0530

    Updated getProfiles and UpdateProfile routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -12,7 +12,7 @@ const authUser = asyncHandler(async (req, res) => {
   const user = await User.findOne({ email });
   if (user && (await user.matchPassword(password))) {
     generateToken(res, user._id);
-    res.json({
+    res.status(200).json({
       _id: user._id,
       name: user.name,
       email: user.email,
@@ -78,7 +78,18 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @access Public
 
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send("Get User Profile");
+    const user = await User.findById(req.user._id);
+    if(user){
+        res.status(200).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        });
+    }else{
+        res.status(401);
+        throw new Error("User not found")
+    }
 });
 
 // @desc Update user profile
@@ -86,7 +97,27 @@ const getUserProfile = asyncHandler(async (req, res) => {
 // @access Private
 
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.send("Update User Profile");
+  const user = await User.findById(req.user._id);
+  if(user){
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if(req.body.password){
+        user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.status(200).json({
+        _id : updatedUser._id,
+        name : updatedUser.name,
+        email : updatedUser.email,
+        isAdmin : updatedUser.isAdmin
+    });
+  }else{
+    res.status(401);
+    throw new Error('User not found');
+  }
 });
 
 // @desc Get users
--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -18,7 +18,7 @@ const errorHandler = (err,req,res,next)=>{
     res.status(statusCode).json({
         message,
         stack : process.env.NODE_ENV === 'production' ? '🍰' : err.stack
-    })
+    });
 }
 
 export {notFound, errorHandler};
