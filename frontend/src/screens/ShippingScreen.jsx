1b390c8c314e254e1da6d898843d6caef1e6c820
karlapraveenkumar <131789praveen@gmail.com>
2025-11-16 19:05:07 +0530

    ShippingScreen Added

---
+++ frontend/src/screens/ShippingScreen.jsx
@@ -0,0 +1,17 @@
+import {useState} from 'react'
+import {Form, Button} from "react-bootstrap";
+import FormContainer from "../components/FormContainer";
+
+const ShippingScreen = () => {
+    const [address, setAddress] = useState('');
+    const [city, setCity] = useState('');
+    const [postalCode, setPostalCode] = useState('');
+    const [country, setCountry] = useState('');
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ShippingScreen;
