ee9b7e26bb3a650c792ceb56e73623defb8fa6d7
karlapraveenkumar <131789praveen@gmail.com>
2025-11-22 15:01:38 +0530

    Integrated the PayPal Account

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -21,6 +21,9 @@ import ShippingScreen from './screens/ShippingScreen.jsx';
 import PrivateRoute from './components/PrivateRoute.jsx';
 import PaymentScreen from './screens/PaymentScreen.jsx';
 import PlaceOrderScreen from './screens/PlaceOrderScreen.jsx';
+import OrderScreen from './screens/OrderScreen.jsx';
+import ProfileScreen from './screens/ProfileScreen.jsx';
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 
 
 const router = createBrowserRouter(
@@ -36,6 +39,8 @@ const router = createBrowserRouter(
         <Route path='/shipping' element={<ShippingScreen/>}/>
         <Route path='/payment' element={<PaymentScreen/>}/>
         <Route path='/placeorder' element={<PlaceOrderScreen/>}/>
+        <Route path='/order/:id' element={<OrderScreen/>}/>
+        <Route path='/profile' element={<ProfileScreen/>}/>
       </Route>
     </Route>
   )
@@ -44,7 +49,9 @@ const router = createBrowserRouter(
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PayPalScriptProvider deferLoading={true}>
+        <RouterProvider router={router} />
+      </PayPalScriptProvider>
     </Provider>
   </StrictMode>,
 )
