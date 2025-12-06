7671c12ed9197a1f71b81adbe708b52ab9d1b296
karlapraveenkumar <131789praveen@gmail.com>
2025-12-06 11:13:46 +0530

    code for Reviews section

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 const router = express.Router();
-import {getProducts, getProductById, createProduct, updateProduct, deleteProduct} from "../controllers/productController.js";
+import {getProducts, getProductById, createProduct, updateProduct, deleteProduct, createProductReview} from "../controllers/productController.js";
 import {protect, admin} from "../middleware/authMiddleware.js";
 
 /*
@@ -17,5 +17,6 @@ router.get('/:id', asyncHandler(async(req,res)=>{
 }));
 */
 router.route('/:id').get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
+router.route('/:id/reviews').post(protect, createProductReview);
 
 export default router;
